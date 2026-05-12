e99808b06c81739e40261e5a3ec2dd8d9276fb09
Bagas Aryo <bagas.priyatama04@gmail.com>
2026-05-12 22:00:07 +0700

    feat: add read data product

--- resources/js/pages/products/index.vue
+++ resources/js/pages/products/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import Button from '@/components/ui/button/Button.vue';
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem, type SharedData } from '@/types';
 import { Head, Link, usePage } from '@inertiajs/vue3';
@@ -15,9 +16,18 @@ const breadcrumbs: BreadcrumbItem[] = [
 
 const page = usePage<SharedData>();
 
-const props = defineProps<{
-    name?: string;
-}>();
+interface Products {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+}
+
+interface Props {
+    products: Products[];
+}
+
+const props = defineProps<Props>();
 </script>
 
 <template>
@@ -27,16 +37,43 @@ const props = defineProps<{
         <div class="p-4">
             <div v-if="page.props.flash?.message" class="mb-4">
                 <Alert class="bg-blue-500 text-white">
-                    <Rocket class="w-6 h-6 mr-2" />
+                    <Rocket class="mr-2 h-6 w-6" />
                     <AlertTitle>Notification!</AlertTitle>
                     <AlertDescription>
                         {{ page.props.flash.message }}
                     </AlertDescription>
                 </Alert>
             </div>
-            <Link :href="route('products.create')">
-                <Button>Create Product page</Button>
-            </Link>
+            <div>
+                <Link :href="route('products.create')">
+                    <Button>Create Product page</Button>
+                </Link>
+            </div>
+            <div>
+                <Table>
+                    <TableCaption>A List of Your Products</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead class="w-[100px]">ID</TableHead>
+                            <TableHead>Name</TableHead>
+                            <TableHead>Price</TableHead>
+                            <TableHead>Description</TableHead>
+                            <TableHead class="text-center">Action</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow v-for="product in props.products" :key="product.id">
+                            <TableCell class="font-medium">{{ product.id }}</TableCell>
+                            <TableCell>{{ product.name }}</TableCell>
+                            <TableCell>{{ product.price }}</TableCell>
+                            <TableCell>{{ product.description }}</TableCell>
+                            <TableCell class="text-center">
+                                Edit | Delete
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </div>
         </div>
     </AppLayout>
 </template>
